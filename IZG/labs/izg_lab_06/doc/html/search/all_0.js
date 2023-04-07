e785e8420f25fbc29b03774f7cb2ae6876400296
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-04-08 00:57:44 +0200

    fix(IZG): keep only task files

--- IZG/labs/izg_lab_06/doc/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['izg_20lab06_2e_0',['Izg lab06.',['../index.html',1,'']]]
-];
--- IZG/labs/izg_lab_06/doc/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['izg_20lab06_2e_1',['Izg lab06.',['../index.html',1,'']]]
-];
